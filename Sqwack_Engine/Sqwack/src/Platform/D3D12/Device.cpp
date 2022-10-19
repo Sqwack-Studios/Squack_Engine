eb4aaf4a5a9c602b15197c44e47252b0dc17f5e2
wildpaco <danielcrespo63@gmail.com>
2022-10-19 02:21:11 +0200

    some refactoring

---
+++ Sqwack_Engine/Sqwack/src/Platform/D3D12/Device.cpp
@@ -0,0 +1,104 @@
+﻿/*===============================================================================/
+/ Sqwack Studios, 2022															 /
+/===============================================================================*/
+//==============================================================================//
+// Name				: Device.cpp										
+// Author			: Dani										 
+// Version			:														     
+// Creation time	: 17/10/2022 17:56:17													 
+//==============================================================================//
+
+#include "pch.h"
+
+#ifndef _Sqwack_Engine_Device_CPP_
+#define _Sqwack_Engine_Device_CPP_
+
+#pragma once
+#include "Device.h"
+#include "Adapter.h"
+
+
+Device::Device() :
+	m_ID3DDevice(nullptr),
+	m_dxgiFactory4(nullptr),
+	m_RtvDescriptorSize(0),
+	m_DsvDescriptorSize(0),
+	m_CbvSrvDescriptorSize(0)
+{
+
+}
+
+Device::~Device()
+{
+
+}
+
+
+bool Device::Init()
+{
+	if (!CreateFactory())
+		return false;
+
+	m_Adapter = Adapter::CreateAdapter(m_dxgiFactory4);
+	if (!m_Adapter)
+		return false;
+
+	if (!CreateDevice())
+		return false;
+
+	
+	return false;
+}
+
+bool Device::CreateFactory()
+{
+	DXCall(HRESULT hr_ = CreateDXGIFactory1(IID_PPV_ARGS(&m_dxgiFactory4)), hr_);
+
+	
+	return (hr_ >= 0);
+}
+
+bool Device::EnableDebugLayer()
+{
+	Microsoft::WRL::ComPtr<ID3D12Debug>	debugController;
+
+	DXCall(HRESULT hr_ = D3D12GetDebugInterface(IID_PPV_ARGS(&debugController)), hr_);
+
+	debugController->EnableDebugLayer();
+
+	return (hr_ >= 0);
+}
+
+bool Device::CheckFeatureSupport()
+{
+	return false;
+}
+
+bool Device::CreateDevice()
+{
+#ifdef	SQWACK_DEBUG
+	//Enable DX12 debug layer
+	if (!EnableDebugLayer())
+		return false;
+#endif
+
+	auto dxgiAdapter4{m_Adapter->GetDXGIAdapter()};
+
+	DXCall(HRESULT hr_ = D3D12CreateDevice(dxgiAdapter4.Get(), D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&m_ID3DDevice)), hr_);
+
+	return (hr_ >= 0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // !_Sqwack_Engine_Device_CPP_
+
---
+++ Sqwack_Engine/Sqwack/src/Platform/D3D12/Device.h
@@ -0,0 +1,71 @@
+﻿/*===============================================================================/
+/ Sqwack Studios, 2022															 /
+/===============================================================================*/
+//==============================================================================//
+// Name				: Device.h											 
+// Author			: Dani										
+// Version			:														     
+// Creation time	: 15/10/2022 14:44:25													 
+//==============================================================================//
+
+#ifndef _Sqwack_Engine_Device_H_
+#define _Sqwack_Engine_Device_H_
+
+#pragma once
+#include "Sqwack/Core/Core.h"
+#include "D3D12Headers.h"
+#include "D3D12LogAssertion.h"
+
+class Adapter;
+
+class Device
+{
+public:
+
+	Device();
+	Device(const Device& device) = delete;
+	Device& operator=(const Device& device) = delete;
+	~Device();
+
+	bool Init();
+
+public:
+
+	inline const Microsoft::WRL::ComPtr<IDXGIFactory4>& GetFactory4() const { return m_dxgiFactory4; }
+
+private: 
+
+	inline bool CreateFactory();
+	bool CheckFeatureSupport();
+	bool CreateDevice();
+	bool EnableDebugLayer();
+
+
+private:
+
+	Microsoft::WRL::ComPtr<IDXGIFactory4>				m_dxgiFactory4;
+
+	Microsoft::WRL::ComPtr<ID3D12Device>				m_ID3DDevice;
+	Microsoft::WRL::ComPtr<ID3D12Fence>					m_ID3DFence;
+
+	UINT												m_RtvDescriptorSize;
+	UINT												m_DsvDescriptorSize;
+	UINT												m_CbvSrvDescriptorSize;
+
+	std::shared_ptr<Adapter>                            m_Adapter;
+
+	
+
+
+
+	
+	
+
+
+private:
+
+
+
+};
+#endif // !_Sqwack_Engine_Device_H_
+
